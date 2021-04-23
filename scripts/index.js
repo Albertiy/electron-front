64971dbfca4b794972bac435ed231ae58ef97631
Albert/袁悦 <YUANYUE963@outlook.com>
2021-04-23 16:14:41 +0800

    [2021年4月23日 16:14:10] 拖动选择图片，显示预览图

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,98 @@
 /** @constant 打开图片按钮 @type HTMLInputElement */
 const imageInput = document.getElementById('image_input');
+/** 原图预览容器 */
 let srcImageContainer = document.getElementById('src_img_container');
+/** 原图预览
+ @type {HTMLImageElement} */
 let srcImage = document.getElementById('src_img');
+/** 方便触发器修改页面值 */
+let pageModel = {
+    _srcImageUrl: '',
+    /** 原图路径 */
+    get srcImageUrl() {
+        return this._srcImageUrl
+    },
+    set srcImageUrl(val) {
+        if (this._srcImageUrl != val) {
+            srcImage.set = val
+            this._srcImageUrl = val
+        }
+
+    },
+}
 
 window.onload = function () {
     console.log('[index.js: onload]');
+    Particles.init({
+        selector: '.background',
+        maxParticles: 450,
+        // options for breakpoints
+        responsive: [
+            {
+                breakpoint: 768,
+                options: {
+                    maxParticles: 56,
+                    color: '#6F6F6F',
+                    connectParticles: true
+                }
+            }, {
+                breakpoint: 425,
+                options: {
+                    maxParticles: 100,
+                    connectParticles: true
+                }
+            }, {
+                breakpoint: 320,
+                options: {
+                    maxParticles: 0
+                    // disables particles.js
+                }
+            }
+        ]
+    });
     imageInput.addEventListener('change', imageInputChanged);
+
+    /**
+    * 页面禁用拖拽上传时 浏览器默认打开图片
+    */
+    document.addEventListener('drop', function (e) {
+        e.preventDefault()
+    }, false)
+    document.addEventListener('dragover', function (e) {
+        e.preventDefault()
+    }, false)
     // 文件拖拽进入
-    srcImage.addEventListener('dragenter', event => {
-        console.log('[EVENT] - dragenter -');
+    srcImageContainer.addEventListener('dragenter', event => {
+        console.log('[EVENT - dragenter]');
         switchClass(srcImageContainer, 'dragover', true);
     }, false);
-
     // 文件拖拽离开
-    srcImage.addEventListener('dragleave', event => {
-        console.log('[EVENT] - dragleave -');
-        if (srcImageContainer.classList.contains('dragover'))
-            srcImageContainer.classList.remove('dragover');
+    srcImageContainer.addEventListener('dragleave', event => {
+        console.log('[EVENT - dragleave]');
+        switchClass(srcImageContainer, 'dragover', false);
     }, false);
-
-    srcImage.addEventListener('dragend', event => {
-        console.log('[EVENT] - dragleave -');
-        if (srcImageContainer.classList.contains('dragover'))
-            srcImageContainer.classList.remove('dragover');
-    }, false);
-
     // 文件拖拽放下
-    srcImage.addEventListener('drop', event => {
-        console.log('[EVENT] - drop -');
-        event.preventDefault();
-        event.stopPropagation();
+    srcImageContainer.addEventListener('drop', event => {
+        console.log('[EVENT - drop]');
+        switchClass(srcImageContainer, 'dragover', false);
+        let df = event.dataTransfer;
+        let dropFiles = [];
+        if (df.items != undefined) {
+            // Chrome有items属性，对Chrome内核的单独处理
+            for (var i = 0; i < df.items.length; i++) {
+                var item = df.items[i];
+                // 用webkitGetAsEntry禁止上传目录
+                if (item.kind === "file" && item.webkitGetAsEntry().isFile) {
+                    var file = item.getAsFile();
+                    dropFiles.push(file);
+                }
+            }
+        }
+        console.log('dropFiles: ', dropFiles)
+        if (dropFiles.length > 0) {
+            html5FileReader(dropFiles[0], res => { setPreviewImage(srcImage, res) });
+        }
+
         return false;
     }, false);
 }
@@ -41,12 +104,21 @@ window.onload = function () {
 function imageInputChanged(event) {
     let image_path = '';
     image_path = imageInput.value;
+    console.log('image_path', image_path);
     if (image_path.indexOf('fakepath') != '-1') {
-        console.log(imageInput.files[0].path);  // 获取真实路径
-        image_path = imageInput.files[0].path;
+        console.log(imageInput.files);  // 获取真实路径
+        let f0 = imageInput.files[0].path;
+        if (f0 && f0 != '') image_path = f0;
     }
-    console.log(image_path);
-    window.myAPI.runPhoto2cartoon(image_path, changeFileName(image_path, generateUUID()));
+    if (image_path && image_path != '') {   // 若路径不为空
+        console.log('image_path', image_path);
+        pageModel.srcImageUrl = image_path;
+        html5FileReader(imageInput.files[0], res => {
+            setPreviewImage(srcImage, res);
+        })
+        // 调用 contextBridge 提供的 API
+        window.myAPI.runPhoto2cartoon(image_path, changeFileName(image_path, generateUUID()));
+    }
 }
 
 
@@ -80,3 +152,37 @@ function switchClass(ele, className, state) {
     else if (!state && ele.classList.contains(className))
         ele.classList.remove(className);
 };
+
+/**
+ * 文件读取
+ * @param {File} file 获取到的文件对象
+ * @param {function(res)} loaded 回调函数
+ */
+function html5FileReader(file, loaded) {
+    try {
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function (e) {
+            console.log(this)
+            loaded(this.result)
+        }
+        return true;
+    } catch (e) {
+        console.log('[html5FileReader ERROR]', e)
+        return false;
+    }
+}
+
+/**
+ * 设置预览图
+ * @param {HTMLImageElement} ele 
+ * @param {*} image 
+ */
+function setPreviewImage(ele, image) {
+    ele.src = image;
+    // 'url(' + this.result + ')'
+}
+
+function afterDropListener() {
+
+}
